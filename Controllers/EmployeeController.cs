c694c39c446647475720a32c471d642508217779
Shweta-Equitec <Shweta.equitec@gmail.com>
2021-11-16 16:33:35 +0530

    Add project files.

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,90 @@
+﻿using DapperMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Dapper;
+
+namespace DapperMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private EmployeeRepository repository;
+
+        public EmployeeController()
+        {
+            repository = new EmployeeRepository();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(repository.GetAll());
+        }
+
+        // GET: Employee/Details/5
+        public ActionResult Details(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        // GET: Employee/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Employee/Create
+        [HttpPost]
+        public ActionResult Create(Employees employee)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Create(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        // GET: Employee/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View(repository.Get(id));
+        }
+
+        // POST: Employee/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Employees employee)
+        {
+            if (ModelState.IsValid)
+            {
+                repository.Update(employee);
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        // GET: Employee/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Employee/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/EmployeeRepository.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using Dapper;
+
+namespace DapperMVC.Models
+{
+    public class EmployeeRepository
+    {
+       
+        private static string connectionString;
+        public EmployeeRepository()
+        {
+            connectionString = System.Configuration.ConfigurationManager.ConnectionStrings["ConStr"].ConnectionString;
+        }
+        public List<Employee> GetAll()
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+                return db.Query<Employee>("sp_Employees_GetAll", commandType: CommandType.StoredProcedure).ToList();
+        }
+
+        public Employee Get(int Id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+                return db.Query<Employee>("sp_Employees_Get", new { Id }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+        }
+
+        public Employee Create(Employee emp)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+                return db.Query<Employee>("sp_Employees_Create", emp, commandType: CommandType.StoredProcedure).FirstOrDefault();
+        }
+
+        public Employee Update(Employee emp)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+                return db.Query<Employee>("Employees_Update", emp, commandType: CommandType.StoredProcedure).FirstOrDefault();
+        }
+        public Employee Delete(int Id)
+        {
+            using (IDbConnection db = new SqlConnection(connectionString))
+                return db.Query<Employee>("sp_Employees_Delete", new { Id }, commandType: CommandType.StoredProcedure).FirstOrDefault();
+        }
+
+
+
+        public static void ExecuteWithoutReturn(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                con.Execute(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+
+        public static T ExecuteReturnScalar<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                return (T)Convert.ChangeType(con.ExecuteScalar(procedureName, param, commandType: CommandType.StoredProcedure), typeof(T)); ;
+            }
+        }
+
+        public static IEnumerable<T> ReturnList<T>(string procedureName, DynamicParameters param = null)
+        {
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                return con.Query<T>(procedureName, param, commandType: CommandType.StoredProcedure);
+            }
+        }
+    }
+}
